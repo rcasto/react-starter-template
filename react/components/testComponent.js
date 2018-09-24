05e628ec08b955d6624fe2a3db4a1734d320ca0e
Richard Casto <richardcasto@Richards-MacBook-Pro.local>
2018-09-23 19:05:08 -0700

    adjust test component

--- react/components/testComponent.js
+++ react/components/testComponent.js
@@ -8,13 +8,26 @@ export default class TestComponent extends Component {
         };
     }
     render() {
-        return createElement(
+        const sourceInfoContainer = createElement(
+            'div',
+            null,
+            `Powered by ${this.props.source}`
+          )
+        const countButton = createElement(
             'button',
             {
                 onClick: () =>
                     this.setState({ clicks: this.state.clicks + 1 })
             },
-            `${this.state.clicks} clicks - ${this.props.source}`
+            `${this.state.clicks} click${this.state.clicks === 1 ? '' : 's'}`
         );
+        return createElement(
+            'div',
+            null,
+            sourceInfoContainer,
+            countButton
+          );
+
+        return ;
     }
 }
