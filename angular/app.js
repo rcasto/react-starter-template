8d7bfbbb0040ba216744cef56b48e6320eeb4fff
Richard Casto <richardcasto@Richards-MacBook-Pro.local>
2018-09-23 19:09:47 -0700

    add test angular service

--- angular/app.js
+++ angular/app.js
@@ -2,4 +2,9 @@ angular
     .module('app', ['react-app'])
     .controller('AppController', function ($scope, testService) {
         $scope.testData = testService.getTestData();
+    })
+    .service('angularService', function () {
+        this.testAngularMethod = function () {
+            return 'angularjs service called';
+        };
     });
