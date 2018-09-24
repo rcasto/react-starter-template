2f15b7541282604bebd3984244a9749f6942b5bf
Richard Casto <richardcasto@Richards-MacBook-Pro.local>
2018-09-23 19:42:59 -0700

    adjust folder structure

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
     mode: 'development',
     devtool: 'inline-source-map',
-    entry: './react/react.app.js',
+    entry: './src/react/react.app.js',
     output: {
         filename: 'react.app.js',
         path: path.resolve(__dirname, 'dist')
