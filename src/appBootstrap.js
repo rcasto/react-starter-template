266c00e244ab8d0c3025d3ec1f73df7b9c12886e
Richard Casto <richardcasto@Richards-MBP.hsd1.md.comcast.net>
2018-10-02 09:14:48 -0400

    refactor bootstrap slightly

--- src/appBootstrap.js
+++ src/appBootstrap.js
@@ -2,9 +2,11 @@ import { createElement } from 'react';
 import { render } from 'react-dom';
 import App from './components/app';
 
-export function bootstrap(selector) {
+export function bootstrap(selector, appState) {
     const domContainer = document.querySelector(selector);
-    render(createElement(App, {
-        iconSrc: 'https://compass-ssl.microsoft.com/assets/58/34/5834c9fa-22c8-41ca-bd03-ef6a4faa7823.png?n=VA%20Avatar%20Revision%20-%20White%20Small%402x.png'
-    }), domContainer);
+    if (!domContainer) {
+        console.error(`Couldn't find an element matching selector ${selector}`);
+        return;
+    }
+    render(createElement(App, appState), domContainer);
 }
--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import { bootstrap } from './appBootstrap';
 import './index.css';
 
-bootstrap('.react-container');
+bootstrap('.react-container', {
+    iconSrc: 'https://compass-ssl.microsoft.com/assets/58/34/5834c9fa-22c8-41ca-bd03-ef6a4faa7823.png?n=VA%20Avatar%20Revision%20-%20White%20Small%402x.png'
+});
