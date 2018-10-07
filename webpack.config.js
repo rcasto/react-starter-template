0bc77d29e86058e2ec9b9c8c891ad829e31cac62
Richard Casto <richardcasto@Richards-MBP.hsd1.md.comcast.net>
2018-10-07 09:48:43 -0400

    add sass and change all css files to scss files

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
 const path = require('path');
 
 module.exports = {
-    mode: 'development',
     devtool: 'inline-source-map',
     entry: './src/public/index.js',
     output: {
@@ -12,7 +11,17 @@ module.exports = {
         rules: [{
             test: /\.css$/,
             // applied last to first (right to left)
-            use: [ 'style-loader', 'css-loader' ]
+            use: [
+                'style-loader', 
+                'css-loader' 
+            ]
+        }, {
+            test: /\.scss$/,
+            use: [
+                "style-loader", // creates style nodes from JS strings
+                "css-loader", // translates CSS into CommonJS
+                "sass-loader" // compiles Sass to CSS, using Node Sass by default
+            ]
         }, {
             test: /\.m?js$/,
             exclude: /(node_modules|bower_components|dist)/,
